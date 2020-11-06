be85f8cdbc1186252150f56cf668813928745e48
Chris Conner <chris.conner57@gmail.com>
2020-11-05 21:24:30 -0800

    Changed env port

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const SECRET_KEY = process.env.SECRET_KEY || 'Rhodes';
 
-const PORT = +process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 let DB_URI;
 
