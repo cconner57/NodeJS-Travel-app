6cdea18e5a37d26b02abbd5d8cda7564e13634d1
Chris Conner <chris.conner57@gmail.com>
2020-11-05 21:47:08 -0800

    Changed git

---
+++ config.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const SECRET_KEY = process.env.SECRET_KEY || 'Rhodes';
+
+const PORT = process.env.PORT || 5000;
+
+let DB_URI;
+
+if (process.env.NODE_ENV === 'Rhodes') {
+	DB_URI = 'travel-test';
+} else {
+	DB_URI = process.env.DATABASE_URL || 'travel';
+}
+
+const contactUser = 'covidtrackerapp1@gmail.com'
+const contactPass = 'wynca6-jahveb-Dezqeh'
+
+const BCRYPT_WORK_FACTOR = 12;
+
+module.exports = {
+	SECRET_KEY,
+	PORT,
+	DB_URI,
+	BCRYPT_WORK_FACTOR,
+	contactUser,
+	contactPass
+};
