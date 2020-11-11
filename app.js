95ee5676ca430d26958e3f603c3b5572865fb033
Chris Conner <chris.conner57@gmail.com>
2020-11-10 17:38:06 -0800

    Changed error

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ app.use('/discussion', discussionRoutes);
 app.use(function (err, req, res, next) {
 	res.status(err.status || 500);
 	console.error(err.stack);
-	console.log('error???')
-	
+	console.log(req)
+
 	return res.json({
 		status: err.status,
 		message: err.message,
