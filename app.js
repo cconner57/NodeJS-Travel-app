18058fc05d6cd9873bb904ef145f728a595158ad
Chris Conner <chris.conner57@gmail.com>
2020-11-10 17:33:13 -0800

    Testing error

--- app.js
+++ app.js
@@ -26,7 +26,8 @@ app.use('/discussion', discussionRoutes);
 app.use(function (err, req, res, next) {
 	res.status(err.status || 500);
 	console.error(err.stack);
-
+	console.log('error???')
+	
 	return res.json({
 		status: err.status,
 		message: err.message,
