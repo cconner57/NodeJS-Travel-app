e7d38d525fe4715c60e132819d5b40800b37d95c
Chris Conner <chris.conner57@gmail.com>
2020-11-10 19:33:16 -0800

    Updated DB

--- db/index.js
+++ db/index.js
@@ -1,6 +1,10 @@
 const { Pool } = require('pg');
 
-const pool = new Pool();
+const connectionString = 'postgres://opjkvgvplsoysy:b278d4f3bbb534bce9e9e4a6edcc0dbf64e9333c705e92f2f216d3c2c2dca507@ec2-54-165-36-134.compute-1.amazonaws.com:5432/d16284dokji902'
+
+const pool = new Pool({
+	connectionString: connectionString,
+});
 module.exports = {
 	query: (text, params) => pool.query(text, params),
 };
